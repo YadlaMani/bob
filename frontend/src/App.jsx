f4481d09ba5e898305c7282c0d46a7de1abe3a00
YadlaMani <yadlamaniymn2005@gmail.com>
2025-01-21 23:12:01 +0530

    added web wallet

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,58 @@
 // src/App.jsx
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import {
+  WalletModalProvider,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
+import {
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+  TorusWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+import { clusterApiUrl } from "@solana/web3.js";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import CreateQuestion from "./pages/createQuestion";
-import { Toaster } from "sonner";
 import AnswerQuestion from "./pages/AnswerQuestion";
 import Home from "./pages/Home";
+import { Toaster } from "sonner";
 import "./index.css";
+import "@solana/wallet-adapter-react-ui/styles.css";
 
 const App = () => {
+  const network = clusterApiUrl("devnet");
+  const wallets = [
+    new PhantomWalletAdapter(),
+    new SolflareWalletAdapter(),
+    new TorusWalletAdapter(),
+  ];
+
   return (
-    <Router>
-      <Toaster />
-      <div>
-        <h1>App</h1>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/create" element={<CreateQuestion />} />
-          <Route path="/answer/:id" element={<AnswerQuestion />} />
-        </Routes>
-      </div>
-    </Router>
+    <ConnectionProvider endpoint={network}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <Router>
+            <Toaster />
+            <div>
+              <h1>App</h1>
+              <WalletMultiButton />
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/create" element={<CreateQuestion />} />
+                <Route path="/answer/:id" element={<AnswerQuestion />} />
+              </Routes>
+            </div>
+          </Router>
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   );
 };
 
