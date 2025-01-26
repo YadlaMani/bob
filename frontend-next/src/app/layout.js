e36bdc2fb4ef9c4adcc94c6f688967973baf61ce
YadlaMani <yadlamaniymn2005@gmail.com>
2025-01-26 21:24:04 +0530

    added some changes

--- frontend-next/src/app/layout.js
+++ frontend-next/src/app/layout.js
@@ -1,19 +1,18 @@
-"use client";
 import "./globals.css";
 import AppWalletProvider from "@/components/AppWalletProvider";
-import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+
 import { Toaster } from "sonner";
+export const metadata = {
+  title: "Bob",
+  description: "Decentralized data labelling platform",
+};
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="flex flex-col min-h-screen">
         <AppWalletProvider>
           <Toaster />
-          <nav className="flex justify-between items-center p-4 bg-gray-100 shadow-lg">
-            <div className="flex justify-between items-center w-full max-w-4xl mx-auto">
-              <WalletMultiButton />
-            </div>
-          </nav>
+
           <main className="flex-grow">{children}</main>
         </AppWalletProvider>
       </body>
