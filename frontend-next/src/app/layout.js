87dea53bddf8e00fd651560664048a52e32eb808
YadlaMani <yadlamaniymn2005@gmail.com>
2025-01-26 11:32:34 +0530

    added create quest page and wallet errors in next.js frontend

--- frontend-next/src/app/layout.js
+++ frontend-next/src/app/layout.js
@@ -2,11 +2,13 @@
 import "./globals.css";
 import AppWalletProvider from "@/components/AppWalletProvider";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { Toaster } from "sonner";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="flex flex-col min-h-screen">
         <AppWalletProvider>
+          <Toaster />
           <nav className="flex justify-between items-center p-4 bg-gray-100 shadow-lg">
             <div className="flex justify-between items-center w-full max-w-4xl mx-auto">
               <WalletMultiButton />
