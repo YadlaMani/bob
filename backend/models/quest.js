ab565a2df4c0164e1318b0f5b523d1ae2ca94d8e
YadlaMani <yadlamaniymn2005@gmail.com>
2025-01-29 23:18:29 +0530

    updated quest model

--- backend/models/quest.js
+++ backend/models/quest.js
@@ -5,7 +5,12 @@ const QuestionSchema = new Schema({
   options: [{ type: String }],
 });
 const QuestSchema = new Schema({
-  thumbnail: { type: String, required: true},
+  thumbnail: {
+    type: String,
+    required: true,
+    default:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.V5zfZZhnF2epZ7FnbejUxwHaEo%26pid%3DApi&f=1&ipt=fc04d043469e13d2f7d0d6485b5cf9ebaf9769301e67a16f76b8f7ad71000f7e&ipo=images",
+  },
   title: { type: String, required: true },
   description: { type: String },
   questions: [QuestionSchema],
